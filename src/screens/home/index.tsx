3bbe5acdd15be7f4c984a0834d0f5ac778cb4786
Wellyngton-dev <contato@wcarvalho.dev>
2021-10-25 00:26:15 -0300

    create a new Expo app

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+import { Header } from '../../components/Header'
+
+import { styles } from './style'
+
+
+
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Header />
+        </View>
+    )
+}
